07cd6620a077723c45c893cd58162b2cdfd05638
francismariasharon <149961925+francismariasharon@users.noreply.github.com>
2024-02-27 23:41:30 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,12 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
 
-  const token = await hre.ethers.deployContract("MetaToken");
+  const daVinci_ctrt = await hre.ethers.getContractFactory("daVinci");
+  const ctrt=await daVinci_ctrt.deploy();
+  await ctrt.deployed();
 
-  console.log("Token address:", await token.getAddress());
+  console.log("daVinci contract address=", ctrt.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
